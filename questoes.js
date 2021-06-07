4ff36336e4fce10bf5e0b6703989ff589d1eff6c
lara-figueiroa <79462391+lara-figueiroa@users.noreply.github.com>
2021-06-07 11:02:51 -0300

    Arquivo com as questões
    .

---
+++ questoes.js
@@ -0,0 +1,22 @@
+function dizOla(text) {
+ return 'Olá, ${text{!
+}
+
+function operador(num1, num2, callback) {
+console.log(callback(num1, num2))
+}
+
+function verificaDivisao(num, callback) {
+const callbackNumber = callback()
+
+return num % callbackNumber = 0 
+}
+
+function mesNumParaString(num) {
+const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+return months[num - 1]
+}
+
+function verificsMaiorOuIgual(n1, n2, callback) { 
+return callback(n1, n2)
+}
